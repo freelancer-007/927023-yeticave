69ced24d3c3b8c4389c51f212ae726006b3db171
Kirill Malakhov <malahovk@gmail.com>
2018-11-20 22:03:21 +0400

    5 Исправил по замечаниям

--- index.php
+++ index.php
@@ -124,15 +124,15 @@ $lots = [
 
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="<?=$lot["img_url"]; ?>" width="350" height="260" alt="">
+                    <img src="<?=$lot['img_url']; ?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?=$lot["cat"]; ?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$lot["title"]; ?></a></h3>
+                    <span class="lot__category"><?=$lot['cat']; ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$lot['title']; ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?=$lot["price"]; ?><b class="rub">р</b></span>
+                            <span class="lot__cost"><?=$lot['price']; ?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
@@ -142,26 +142,6 @@ $lots = [
             </li>
 
         <?php endforeach; ?>
-
-            <!--заполните этот список из массива с товарами-->
-            <li class="lots__item lot">
-                <div class="lot__image">
-                    <img src="" width="350" height="260" alt="">
-                </div>
-                <div class="lot__info">
-                    <span class="lot__category">Название категории</span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html">Название товара</a></h3>
-                    <div class="lot__state">
-                        <div class="lot__rate">
-                            <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">цена<b class="rub">р</b></span>
-                        </div>
-                        <div class="lot__timer timer">
-                            12:23
-                        </div>
-                    </div>
-                </div>
-            </li>
 
         </ul>
     </section>
