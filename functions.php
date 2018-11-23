8cd166823fa4dbb6b6ac5b98155780735b3d7d5b
Kirill Malakhov <malahovk@gmail.com>
2018-11-24 00:38:49 +0400

    #7 Выполнил шаблонизацию проекта

---
+++ functions.php
@@ -0,0 +1,36 @@
+<?php
+
+function include_template($name, $data) {
+    $name = 'templates/' . $name;
+    $result = '';
+
+    if (!file_exists($name)) {
+        return $result;
+    }
+
+    ob_start();
+    extract($data);
+    require $name;
+
+    $result = ob_get_clean();
+
+    return $result;
+}
+
+
+function format_price( float $price ): string {
+
+    $price_rounded = ceil($price);
+
+    if ( $price_rounded >= 1000 ){
+        $price_string = (string) $price_rounded;
+        $price_start  = substr($price_string, 0, strlen($price_string)-3);
+        $price_end = substr($price_string, -3);
+
+        $price_rounded = $price_start . ' ' . $price_end;
+    }
+
+    $price_full = $price_rounded . '<b class="rub">ð</b>';
+
+    return  $price_full;
+}
